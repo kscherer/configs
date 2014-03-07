9cb7841a878a8e678ba66dc46820823e7de345aa
Konrad Scherer <Konrad.Scherer@windriver.com>
2014-03-07 14:06:36 -0500

    Add snippets for the modes I use regularly
    Signed-off-by: Konrad Scherer <Konrad.Scherer@windriver.com>

--- emacs.d/emacs_config/custom.el
+++ emacs.d/emacs_config/custom.el
@@ -3,7 +3,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(flymake-run-in-place nil)
  '(ibuffer-expert t)
  '(sh-indent-comment t)
  '(tab-width 4)
--- emacs.d/emacs_config/init-company-mode.el
+++ emacs.d/emacs_config/init-company-mode.el
@@ -2,3 +2,4 @@
 (setq company-idle-delay 0.3)
 (add-to-list 'company-backends 'company-cider)
 (add-to-list 'company-backends 'company-clang)
+(add-to-list 'company-backends 'company-capf)
