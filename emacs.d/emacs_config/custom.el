95754cfb2ff73530ac7dbfc90892e61c74fd30a1
Konrad Scherer <Konrad.Scherer@windriver.com>
2014-10-06 16:59:48 -0400

    Just follow symlinks without asking

--- emacs.d/emacs_config/custom.el
+++ emacs.d/emacs_config/custom.el
@@ -6,7 +6,8 @@
  '(blink-matching-delay 0.1)
  '(ibuffer-expert t)
  '(sh-indent-comment t)
- '(tab-width 4))
+ '(tab-width 4)
+ '(vc-follow-symlinks t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
