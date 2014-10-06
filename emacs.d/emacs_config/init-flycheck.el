3e2cb0082ab2d4dd2d06d11d004f7daafa69161b
Konrad Scherer <Konrad.Scherer@windriver.com>
2014-10-06 17:42:52 -0400

    Disable eldoc checker

--- emacs.d/emacs_config/init-flycheck.el
+++ emacs.d/emacs_config/init-flycheck.el
@@ -1,3 +1,4 @@
 (require 'flycheck)
 ;; Run pylint if flake8 does not return any errors
 (flycheck-add-next-checker 'python-flake8 'python-pylint)
+(setq-default flycheck-disabled-checkers '(emacs-lisp-checkdoc))
