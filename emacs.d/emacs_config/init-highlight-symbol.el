911e9d1b59397e417474e9862f91d2fadef0305e
Konrad Scherer <Konrad.Scherer@windriver.com>
2016-05-12 14:20:02 -0400

    Remove call to deprectated variable
    Signed-off-by: Konrad Scherer <Konrad.Scherer@windriver.com>

--- emacs.d/emacs_config/init-highlight-symbol.el
+++ emacs.d/emacs_config/init-highlight-symbol.el
@@ -1,4 +1,3 @@
 ;;emulate the vi * search functionality
-(setq highlight-symbol-on-navigation-p t)
 (global-set-key (kbd "C-*") 'highlight-symbol-next)
 (global-set-key (kbd "C-M-*") 'highlight-symbol-prev)
