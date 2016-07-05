dd031ef825c224d2976d66db2fa6cd5d47145aed
Konrad Scherer <Konrad.Scherer@windriver.com>
2016-07-05 16:46:39 -0400

    Add which-key-mode
    Signed-off-by: Konrad Scherer <Konrad.Scherer@windriver.com>

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -33,7 +33,7 @@
          rfringe flycheck puppet-mode
          edit-server ledger-mode color-theme-solarized expand-region
          key-chord pp-c-l yaml-mode multiple-cursors
-         avy clojure-mode s cider
+         avy clojure-mode s cider which-mode
          paredit rainbow-delimiters markdown-mode flx helm
          queue cl-lib jedi virtualenvwrapper
          company-mode pymacs beacon hungry-delete
