d50f2d0636fb46259e4b07bb2be4ec51f97c32d7
Konrad Scherer <Konrad.Scherer@windriver.com>
2017-04-11 12:32:32 -0400

    Set emacs default font to Noto Mono
    Signed-off-by: Konrad Scherer <Konrad.Scherer@windriver.com>

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,4 +1,5 @@
 ;; Turn off mouse interface early in startup to avoid momentary display
+(set-default-font "Noto Mono-10")
 (if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
