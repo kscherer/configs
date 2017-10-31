121d30021353eb832717a2e1f2e5003f86a53b0e
Konrad Scherer <Konrad.Scherer@windriver.com>
2017-10-31 17:25:23 -0400

    Add emacs load-path to flycheck elisp load path to remove warnings in init files

--- emacs.d/emacs_config/init-flycheck.el
+++ emacs.d/emacs_config/init-flycheck.el
@@ -1,4 +1,5 @@
 (require 'flycheck)
+(setq-default flycheck-emacs-lisp-load-path 'inherit)
 ;; Run pylint if flake8 does not return any errors
 (flycheck-add-next-checker 'python-flake8 'python-pylint)
 (setq-default flycheck-disabled-checkers '(emacs-lisp-checkdoc))
